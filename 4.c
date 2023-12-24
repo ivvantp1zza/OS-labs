86cef5eb6a06d2b50c472293dd020fbb12dca50f
ivvantp1zza <108880743+ivvantp1zza@users.noreply.github.com>
2023-12-24 16:03:04 +0300

    task 4 done

---
+++ 4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("wrong args count(should be 2)\n");
+        return 1;
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        printf("cannot open file\n");
+        return 1;
+    }
+
+    int N = atoi(argv[2]);
+
+    if (N < 0) {
+        printf("N must be positive number or 0!\n");
+        return 1;
+    }
+
+    if (N != 0) {
+        char c;
+        int lines = 0;
+        while ((c = fgetc(file)) != EOF) {
+            if (c == '\n') {
+                lines++;
+            }
+            if (lines == N) {
+                fgetc(stdin);
+                lines = 0;
+                continue;
+            }
+            fputc(c, stdout);
+        }
+    }
+    else {
+        char c;
+        while ((c = fgetc(file)) != EOF) {
+            fputc(c, stdout);
+        }
+    }
+    
+    return fclose(file);
+}
